aa470c82545b42e36c4e60ab61f09359b20e6635
Aloke Desai <aloke.desai@gmail.com>
2014-07-13 00:12:47 -0700

    redirect user back to page they were at before signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,20 @@ class SessionsController < ApplicationController
 	def create
 		user = User.from_omniauth(env["omniauth.auth"])
 		session[:user_id] = user.id
-		redirect_to root_url
+		redirect_to back_url
 	end
 
 	def destroy
 		session[:user_id] = nil
 		redirect_to root_url
 	end
+
+	private
+	def back_url
+    if request.env["omniauth.origin"].present?
+      request.env["omniauth.origin"]
+    else
+      root_url
+    end
+  end
 end
